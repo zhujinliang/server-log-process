1948b75fc64eecf563b94f18f77b566bd48c6b98
Jinliang Zhu <zhujinlianghust@gmail.com>
2014-05-16 16:12:24 +0800

    [REV] Update cut nginx log script

--- cut_nginx_log.sh
+++ cut_nginx_log.sh
@@ -1,16 +1,22 @@
 #! /bin/bash
 
-NGINX_LOG_PATH="/www/wdlinux/nginx/logs"
+# NGINX_LOG_PATH="/www/wdlinux/nginx/logs"
+NGINX_LOG_PATH="/home/jzhu/projects/server-log-process"
 
 NGINX_BACK_LOG_PATH=${NGINX_LOG_PATH}/$(date -d "yesterday" +%Y)/$(date -d "yesterday" +%m)
 
-NGINX_BACK_LOG_NAME=$(date -d "yesterday" +%Y%m%d)_access.log
+NGINX_BACK_LOG_NAME_PREFIX=$(date -d "yesterday" +%Y%m%d)_
+
 mkdir -p $NGINX_BACK_LOG_PATH
 
-# 复制老的日志文件
-cp ${NGINX_LOG_PATH}/access.log ${NGINX_BACK_LOG_PATH}/${NGINX_BACK_LOG_NAME}
-echo 1> ${NGINX_LOG_PATH}/access.log
+# Copy old server log
+# gongpingjia.com api.gongpingjia.com log
+for LOG_FILE in "gongpingjia" "mobile"
+do
+cp ${NGINX_LOG_PATH}/${LOG_FILE}_access.log ${NGINX_BACK_LOG_PATH}/${NGINX_BACK_LOG_NAME_PREFIX}${LOG_FILE}_access.log
+echo 1> ${NGINX_LOG_PATH}/${LOG_FILE}_access.log
+done
 
-# nginx重新开一个新的日志
+# Nginx open a new server log file
 # service nginxd restart
 
