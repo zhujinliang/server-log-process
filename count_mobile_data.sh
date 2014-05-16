71e4a6b00166f60dcc85a21781892b3cdbeb9dd9
Jinliang Zhu <zhujinlianghust@gmail.com>
2014-05-16 16:26:49 +0800

    [REV] Update mobile api statistics count

--- count_mobile_data.sh
+++ count_mobile_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-APACHE_LOG_PATH="/www/wdlinux/apache/logs"
-NGINX_LOG_PATH="/www/wdlinux/nginx/logs"
+# NGINX_LOG_PATH="/www/wdlinux/nginx/logs"
+NGINX_LOG_PATH="/home/jzhu/projects/server-log-process"
 
 
 IOS_HEADER="ASIHTTP"
@@ -9,7 +9,7 @@ ANDROID_HEADER="Apache-HttpClient"
 
 IGNORE_IP="(221.6.31.164)|(50.116.6.27).*"
 
-ANDROID_APP_NAME="gongpingjia2.0.apk"
+ANDROID_APP_NAME="gongpingjia.*.apk"
 
 year=$(date -d "yesterday" +%Y)
 month=$(date -d "yesterday" +%m)
@@ -18,8 +18,12 @@ year_month_day=$(date -d "yesterday" +%Y%m%d)
 
 date -d "yesterday" +%Y-%m-%d
 
+LOG_FILE=${NGINX_LOG_PATH}/${year}/${month}/${year_month_day}
+GPJ_LOG_FILE=${LOG_FILE}_gongpingjia_access.log
+MOBILE_LOG_FILE=${LOG_FILE}_mobile_access.log
+
 # 统计二维码扫描次数
-log_file=${APACHE_LOG_PATH}/${year}/${month}/${year_month_day}_access_log
+log_file=${GPJ_LOG_FILE}
 spider_count=$(grep -i "/app/download/.*spider" ${log_file} | cut -d ' ' -f 1 | sort | uniq | wc -l)
 all_scan_count=$(grep -i "/app/download" ${log_file} | cut -d ' ' -f 1 | sort | uniq | wc -l)
 scan_count=$(( ${all_scan_count} - ${spider_count} ))
@@ -28,7 +32,6 @@ echo "二维码扫描次数, ${scan_count}"
 
 
 # 统计Android APP 下载次数
-log_file=${NGINX_LOG_PATH}/${year}/${month}/${year_month_day}_access.log
 spider_download_count=$(grep -i "$ANDROID_APP_NAME.*\(spider\|bot\)" ${log_file} | cut -d ' ' -f 1 | sort | uniq | wc -l)
 all_download_count=$(grep -i ${ANDROID_APP_NAME} ${log_file} | cut -d ' ' -f 1 | sort | uniq | wc -l)
 android_app_download_count=$(( ${all_download_count} - ${spider_download_count} ))
@@ -39,7 +42,7 @@ echo "Android APP 下载次数, ${android_app_download_count}"
 
 # 统计iOS和Android端的访问接口次数
 # 访问估值接口次数
-log_file=${APACHE_LOG_PATH}/${year}mobile/${month}/${year_month_day}_access_log
+log_file=${MOBILE_LOG_FILE}
 evaluate_flag="evaluate/price-data.*"
 android_count=$(grep -c ${evaluate_flag}${ANDROID_HEADER} ${log_file})
 ios_count=$(grep -c ${evaluate_flag}${IOS_HEADER} ${log_file})
@@ -66,7 +69,7 @@ echo "iOS端访问买车接口次数, ${ios_count}"
 
 
 # 短链接访问次数（即通过短信链接访问的人次数）
-log_file=${APACHE_LOG_PATH}/${year}/${month}/${year_month_day}_access_log
+log_file=${GPJ_LOG_FILE}
 short_url_count=$(egrep 'GET /s/\w{5}' ${log_file} | cut -d ' ' -f 8 | sort | uniq | wc -l)
 echo "通过短链接访问的次数, ${short_url_count}"
 
