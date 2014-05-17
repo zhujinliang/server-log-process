a6ee7dd621c0c96c940967cd121c85ceea87a3a0
Jinliang Zhu <zhujinlianghust@gmail.com>
2014-05-17 10:31:22 +0800

    [REV] Update statistic

--- count_mobile_data.sh
+++ count_mobile_data.sh
@@ -23,16 +23,16 @@ MOBILE_LOG_FILE=${LOG_FILE}_mobile_access.log
 
 # 统计二维码扫描次数
 log_file=${GPJ_LOG_FILE}
-spider_count=$(grep -i "/app/download/.*spider" ${log_file} | cut -d ' ' -f 1 | sort | uniq | wc -l)
-all_scan_count=$(grep -i "/app/download" ${log_file} | cut -d ' ' -f 1 | sort | uniq | wc -l)
+spider_count=$(grep -ic "/app/download/.*spider" ${log_file})
+all_scan_count=$(grep -i "/app/download" ${log_file} | cut -d ' ' -f 1 | sed '/,/d' | sort | uniq | wc -l)
 scan_count=$(( ${all_scan_count} - ${spider_count} ))
 
 echo "二维码扫描次数, ${scan_count}"
 
 
 # 统计Android APP 下载次数
-spider_download_count=$(grep -i "$ANDROID_APP_NAME.*\(spider\|bot\)" ${log_file} | cut -d ' ' -f 1 | sort | uniq | wc -l)
-all_download_count=$(grep -i ${ANDROID_APP_NAME} ${log_file} | cut -d ' ' -f 1 | sort | uniq | wc -l)
+spider_download_count=$(grep -ic "$ANDROID_APP_NAME.*\(spider\|bot\)" ${log_file})
+all_download_count=$(grep -i ${ANDROID_APP_NAME} ${log_file} | cut -d ' ' -f 1 | sed '/,/d' | sort | uniq | wc -l)
 android_app_download_count=$(( ${all_download_count} - ${spider_download_count} ))
 
 echo "Android APP 下载次数, ${android_app_download_count}"
@@ -69,7 +69,7 @@ echo "iOS端访问买车接口次数, ${ios_count}"
 
 # 短链接访问次数（即通过短信链接访问的人次数）
 log_file=${GPJ_LOG_FILE}
-short_url_count=$(egrep 'GET /s/\w{5}' ${log_file} | cut -d ' ' -f 8 | sort | uniq | wc -l)
+short_url_count=$(egrep -o 'GET /s/\w{5}' ${log_file} | sort | uniq | wc -l)
 echo "通过短链接访问的次数, ${short_url_count}"
 
 echo
